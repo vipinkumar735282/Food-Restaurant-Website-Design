fe596f80ff3ad8e12d060e19e3cecb2580f0700c
vipin kumar <vipin735282@gmail.com>
2022-08-29 23:39:14 +0530

    first commit

---
+++ css/script.js
@@ -0,0 +1,36 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active');
+}
+
+
+
+var swiper = new swiper(".Home-slider", {
+    spaceBetween:30,
+    cennteredSlides:true,
+    autoplay: {
+        delay:7500,
+        disableOnInteraction:false,
+},
+pagination:{
+    el: ".swiper-paginaton",
+    clickable:true,
+},
+loop:true
+});
